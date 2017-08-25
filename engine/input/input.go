5472baa2652223cb561d8bd1ae93da37c5e75989
thewrath <thomaslegoffpro@gmail.com>
2017-08-25 20:47:43 +0200

    change genDoc system

--- engine/input/input.go
+++ engine/input/input.go
@@ -3,6 +3,7 @@ package input
 // # Package input
 import "github.com/DualGo/glfw/v3.2/glfw"
 
+//- ## Struct Key 
 type Key struct {
 	keycode glfw.Key
 	action  glfw.Action
@@ -16,6 +17,11 @@ const (
 
 var keys []Key
 
+//	- ### GetKey(keycode gflw.key)
+//		- > return the state of a key 
+// 
+//		- > return int
+// 
 func GetKey(keycode glfw.Key) int {
 	for _, element := range keys {
 		if keycode == element.keycode {
@@ -24,10 +30,21 @@ func GetKey(keycode glfw.Key) int {
 	}
 	return 0
 }
+
+//	- ### SetKey(keycode gflw.Key, action gflw.Action)
+//		- > addKey to input system
+// 
+//		- > return void
+// 
 func SetKey(keycode glfw.Key, action glfw.Action) {
 	keys = append(keys, Key{keycode, action})
 }
 
+//	- ### RemoveKeys()
+//		- > clear the input system 
+// 
+//		- > return void
+// 
 func RemoveKeys() {
 	keys = nil
 }
